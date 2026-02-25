72d4fb5873c6214b9afa983c93836527ce73d63e
thedavidcarney <thedavidcarney@gmail.com>
2026-02-25 12:21:46 -0600

    updated playerprefs

--- Assets/DepthThresholdController.cs
+++ Assets/DepthThresholdController.cs
@@ -7,6 +7,9 @@ public class DepthThresholdController : MonoBehaviour
     public float stepSize = 0.5f;
 
     private Material mat;
+    private float saveTimer = 0f;
+    private const float SAVE_INTERVAL = 1.0f;
+    private bool dirty = false;
 
     void Start()
     {
@@ -20,33 +23,42 @@ public class DepthThresholdController : MonoBehaviour
     void Update()
     {
         bool shift = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        bool changed = false;
 
         if (shift)
         {
-            if (Input.GetKey(KeyCode.W)) minDepth += stepSize * Time.deltaTime;
-            if (Input.GetKey(KeyCode.S)) minDepth -= stepSize * Time.deltaTime;
+            if (Input.GetKey(KeyCode.W)) { minDepth += stepSize * Time.deltaTime; changed = true; }
+            if (Input.GetKey(KeyCode.S)) { minDepth -= stepSize * Time.deltaTime; changed = true; }
         }
         else
         {
-            if (Input.GetKey(KeyCode.W)) maxDepth += stepSize * Time.deltaTime;
-            if (Input.GetKey(KeyCode.S)) maxDepth -= stepSize * Time.deltaTime;
+            if (Input.GetKey(KeyCode.W)) { maxDepth += stepSize * Time.deltaTime; changed = true; }
+            if (Input.GetKey(KeyCode.S)) { maxDepth -= stepSize * Time.deltaTime; changed = true; }
         }
 
-        minDepth = Mathf.Clamp(minDepth, 0f, 1f);
-        maxDepth = Mathf.Clamp(maxDepth, 0f, 1f);
-        if (minDepth > maxDepth) minDepth = maxDepth;
+        if (changed)
+        {
+            minDepth = Mathf.Clamp(minDepth, 0f, 1f);
+            maxDepth = Mathf.Clamp(maxDepth, 0f, 1f);
+            if (minDepth > maxDepth) minDepth = maxDepth;
 
-        mat.SetFloat("_MinDepth", minDepth);
-        mat.SetFloat("_MaxDepth", maxDepth);
+            mat.SetFloat("_MinDepth", minDepth);
+            mat.SetFloat("_MaxDepth", maxDepth);
+            dirty = true;
+        }
 
-        if (Time.frameCount % 60 == 0)
-            Debug.Log($"Depth range: {minDepth / 6.5535f * 10000f:F0}mm - {maxDepth / 6.5535f * 10000f:F0}mm");
-    }
-
-    void OnApplicationQuit()
-    {
-        PlayerPrefs.SetFloat("minDepth", minDepth);
-        PlayerPrefs.SetFloat("maxDepth", maxDepth);
-        PlayerPrefs.Save();
+        if (dirty)
+        {
+            saveTimer += Time.deltaTime;
+            if (saveTimer >= SAVE_INTERVAL)
+            {
+                PlayerPrefs.SetFloat("minDepth", minDepth);
+                PlayerPrefs.SetFloat("maxDepth", maxDepth);
+                PlayerPrefs.Save();
+                dirty = false;
+                saveTimer = 0f;
+                Debug.Log($"Saved depth range: {minDepth / 6.5535f * 10000f:F0}mm - {maxDepth / 6.5535f * 10000f:F0}mm");
+            }
+        }
     }
 }
